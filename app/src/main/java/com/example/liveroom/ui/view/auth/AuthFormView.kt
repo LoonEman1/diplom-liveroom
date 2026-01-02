4015f05c3f93fb8b383876aaeab97b312e2e4896
Nikita Plotnikov <pnik10969@gmail.com>
2026-01-03 00:39:52 +0300

    add visibility toggle

--- app/src/main/java/com/example/liveroom/ui/view/auth/AuthFormView.kt
+++ app/src/main/java/com/example/liveroom/ui/view/auth/AuthFormView.kt
@@ -1,7 +1,5 @@
 package com.example.liveroom.ui.view.auth
 
-import androidx.compose.foundation.Indication
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
@@ -10,19 +8,28 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.Lock
+import androidx.compose.material.icons.filled.Person
 import androidx.compose.material3.Button
 import androidx.compose.material3.Checkbox
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.VisualTransformation
@@ -30,6 +37,9 @@ import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.liveroom.data.model.AuthFieldConfig
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.ImeAction
+import com.example.liveroom.R
 
 @Composable
 fun AuthTextField(
@@ -37,8 +47,10 @@ fun AuthTextField(
     value : String,
     onValueChange: (String) -> Unit,
     modifier : Modifier = Modifier,
-    fieldType : String
+    fieldType : String,
 ) {
+    var showPassword by remember { mutableStateOf(false) }
+    var isError by remember { mutableStateOf(false)}
 
     OutlinedTextField(
         value = value,
@@ -49,19 +61,56 @@ fun AuthTextField(
         modifier = modifier
             .fillMaxWidth()
             .padding(horizontal = 16.dp),
-        visualTransformation = if (value == "password") {
+        shape = RoundedCornerShape(16.dp),
+        leadingIcon = {
+            when(fieldType) {
+                "login" -> Icon(Icons.Default.Person, contentDescription = "Login")
+                "email" -> Icon(Icons.Default.Email, contentDescription = "Email")
+                "password" -> Icon(Icons.Default.Lock, contentDescription = "Password")
+            }
+        },
+        trailingIcon = {
+            if (fieldType == "password") {
+                IconButton(
+                    onClick = {
+                        showPassword = !showPassword
+                    }
+                ) {
+                    Icon(
+                        painter = painterResource( id = if(showPassword) {
+                            R.drawable.ic_visibility
+                        } else {
+                            R.drawable.ic_visibility_off
+                        }),
+                        contentDescription = if (showPassword) "Hide" else "Show"
+                    )
+                }
+            }
+        },
+        visualTransformation = if (fieldType == "password" && !showPassword) {
             PasswordVisualTransformation()
         }
         else {
             VisualTransformation.None
         },
-        keyboardOptions = if (fieldType == "password") {
-            KeyboardOptions(keyboardType = KeyboardType.Password)
-        } else if (fieldType == "email") {
-            KeyboardOptions(keyboardType = KeyboardType.Email)
-        } else {
-            KeyboardOptions.Default
-        }
+        keyboardOptions = when (fieldType) {
+            "password" -> KeyboardOptions(
+                keyboardType = KeyboardType.Password,
+                imeAction = ImeAction.Done
+                )
+            "email" -> KeyboardOptions(
+                keyboardType = KeyboardType.Email,
+                imeAction = ImeAction.Next
+            )
+            else -> KeyboardOptions(imeAction = ImeAction.Next
+            )
+        },
+        maxLines = 1,
+        keyboardActions = KeyboardActions(
+            onAny = {
+
+            }
+        )
     )
 }
 
